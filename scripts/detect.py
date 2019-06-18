520d873c7b7d675b5c4da2002a48aa121ff37e06
Josh Veitch-Michaelis <j.veitchmichaelis@gmail.com>
2019-06-18 17:58:03 +0100

    publish Detection2DArray
    Publish appropriate ROS messages for detection results

--- scripts/detect.py
+++ scripts/detect.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import roslib
 roslib.load_manifest("edge_tpu")
@@ -8,7 +8,7 @@ import cv2
 import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-#from object_detection_msgs.msgs import RecognizedObject
+from vision_msgs.msg import Detection2DArray, Detection2D, BoundingBox2D, ObjectHypothesisWithPose
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import PIL
@@ -23,8 +23,9 @@ class tpu_detector:
         self.image_sub = rospy.Subscriber("input", Image, self.callback)
         self.threshold = threshold
         self.engine = edgetpu.detection.engine.DetectionEngine(model, device_path)
-
+        rospy.loginfo("Engine loaded")
         self.load_labels(labels)
+        self.detection_pub = rospy.Publisher('detections', Detection2DArray, queue_size=10)
 
     def load_labels(self, labels):
         with open(labels, 'r', encoding="utf-8") as f:
@@ -38,15 +39,61 @@ class tpu_detector:
             rospy.logerr(e)
 
         results = self.engine.DetectWithImage(PIL.Image.fromarray(cv_image), top_k=1, threshold=self.threshold, keep_aspect_ratio=True, relative_coord=True)
+        
+        detections = Detection2DArray()
+        now = rospy.get_rostime()
 
         for detection in results:
-            try:
-                rospy.loginfo(self.labels[detection.label_id])
-                rospy.loginfo(detection.bounding_box)
-                
-            except:
-                rospy.logerr("Error processing results")
-                rospy.logerr(results)
+        
+            top_left, bottom_right = detection.bounding_box
+            min_x, min_y = top_left
+            max_x, max_y = bottom_right
+            
+            imheight, imwidth, _ = cv_image.shape
+            
+            min_x *= imwidth
+            max_x *= imwidth
+            min_y *= imheight
+            max_y *= imheight
+            
+            centre_x = (max_x+min_x)/2.0
+            centre_y = (max_y+min_y)/2.0
+            height = max_y-min_y
+            width = max_x-min_x
+            
+            if height <=0 or width <= 0:
+              continue
+            
+            bbox = BoundingBox2D()
+            bbox.center.x = centre_x
+            bbox.center.y = centre_y
+            bbox.size_x = width
+            bbox.size_y = height
+            
+            hypothesis = ObjectHypothesisWithPose()
+            hypothesis.id = detection.label_id
+            hypothesis.score = detection.score
+            hypothesis.pose.pose.position.x = centre_x
+            hypothesis.pose.pose.position.y = centre_y
+ 
+            # update the timestamp of the object
+            object = Detection2D()
+            object.header.stamp = now
+            object.header.frame_id = data.header.frame_id
+            object.results.append(hypothesis)
+            object.bbox = bbox
+            object.source_img.header.frame_id = data.header.frame_id
+            object.source_img.header.stamp = now
+            object.source_img.height = int(height)
+            object.source_img.width = int(width)
+            object.source_img.encoding = "rgb8"
+            object.source_img.step = int(width*3)
+            object.source_img.data = cv_image[int(min_y):int(max_y), int(min_x):int(max_x)].tobytes()
+            
+            detections.detections.append(object)
+            
+        if len(results) > 0:
+          self.detection_pub.publish(detections)
         
         rospy.logdebug("%.2f ms" % self.engine.get_inference_time())
 
